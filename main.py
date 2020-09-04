ffcc80dd99d3c00fc06c1710a471c9b72a81c1d1
Rafal Kruk <rkruk@users.noreply.github.com>
2020-09-04 14:47:41 +0100

    Update main.py
    Reviewed code comments. Added some more information to clarify things up.

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ try:
 
     # How to optimise that thing to not ping for weather twice at the same time.
     # hudd1 + hudd2 = hudd something something ... format=?
+    # THIS PART REQUIRE REVIEW AND REDO. See wttr github documentation --> https://github.com/chubin/wttr.in .
+    # The idea is to do one get request at a time for all informations needed and then to splice them into parts.
+    # I'm not a programmer and I know nothing about those things at all - just throwing some randoms hoping for a better results..
     hudd = requests.get('https://wttr.in/Huddersfield?format=%l:+%c+%t+%w&period=60') # +%S (sunrise)
     hudd2 = requests.get('https://wttr.in/Huddersfield?format=%h+%w+%o&period=60') # +%d (dusk)
 
-    # From Demo - leave those for future reference sake:
+    # From Demo - leave those for the future reference sake:
     # epd.Clear(0xFF)
     # epd.display(epd.getbuffer(image))
     # time.sleep(2)
@@ -31,11 +34,12 @@ try:
     print("Show time")
     epd.init(epd.PART_UPDATE)
     # Full update - image refresh is flickering visibly every 1sec. 
-    # Change to full, when you'll figure it out how to change the time interval.
+    # Change to FULL_UPDATE, when you'll figure it out how to change the time interval of that weird refresh.
     #epd.init(epd.FULL_UPDATE) # for reference
     
     # Clear (white) background (TEST THIS BIT - as it seems there may be no need to do that twice (line:15 and 38)!):
     epd.Clear(0xFF)
+    
     # Lets define some fonts:
     font26 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 26)
     font32 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 32)
@@ -43,7 +47,7 @@ try:
     font15 = ImageFont.truetype('/home/pi/.local/share/fonts/NotoColorEmoji.ttf', 15)
     #font15 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 15)
     
-    # Create here an empty image objects/layers:
+    # Create an empty image for objects/layers:
     time_image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)
     # Render the time section:
     time_draw = ImageDraw.Draw(time_image)
@@ -53,7 +57,7 @@ try:
         # show day and day date (left side):
         time_draw.text((10, 10), time.strftime(' %a %d'), font = font26, fill = 0)
         
-        ### Leave that bit from example code for next update in the future (a nice GUI optimisations):
+        ### Leave that bit from example code for next code update in the future (for a nice GUI optimisations):
         # draw.line([(100,10),(100,50)], fill = 0,width = 1)
         ###
 
